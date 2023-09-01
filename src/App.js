2bff6307d2206468b3f9d6d81761d12449fc656c
Maamoorjon <43916273+Maamoorjon@users.noreply.github.com>
2023-09-02 05:23:49 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [val, setVal] = useState(0);
+  const countUp = () => setVal(val + 1);
+  const countDown = () => setVal(val - 1);
+
+  return (
+    <div className="App">
+      <h3>現在のカウント数:{val}</h3>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
+    </div>
+  );
+}
